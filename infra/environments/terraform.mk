2ddd8c35b8696d7eb202398dce162707b2db7171
David Berichon <davidberich@gmail.com>
2024-10-27 11:38:03 +0000

    feat(docker-setup): Update Docker repository and Terraform state configuration

--- infra/environments/terraform.mk
+++ infra/environments/terraform.mk
@@ -20,6 +20,9 @@ terraform-state-list: $(GOOGLE_APPLICATION_CREDENTIALS)
 
 # This list is used to provide generic terraform targets 
 TF_STATE_ITEMS = \
+  data.google_project.project \
+  data.google_secret_manager_secret_version.google_idp_oauth_client_id \
+  data.google_secret_manager_secret_version.google_idp_oauth_client_secret \
   google_identity_platform_default_supported_idp_config.google \
   google_project_service.google_services_default["cloudbilling.googleapis.com"] \
   google_project_service.google_services_default["cloudbuild.googleapis.com"] \
@@ -29,34 +32,44 @@ TF_STATE_ITEMS = \
   google_project_service.google_services_default["iam.googleapis.com"] \
   google_project_service.google_services_default["secretmanager.googleapis.com"] \
   google_project_service.google_services_default["serviceusage.googleapis.com"] \
+  google_service_account_iam_member.user_service_account_binding \
+  google_storage_bucket.bucket_tf_state_eu_global \
+  module.cdn.google_compute_backend_bucket.public_cdn \
+  module.cdn.google_compute_global_address.public_cdn \
+  module.cdn.google_compute_global_forwarding_rule.public_cdn \
+  module.cdn.google_compute_target_http_proxy.public_cdn \
+  module.cdn.google_compute_url_map.url_map \
+  module.cdn.google_storage_bucket_iam_binding.web_public_image \
+  module.cdn.google_storage_bucket_object.public_web_background_image \
+  module.cdn.google_storage_bucket.public_images \
   module.gcloud.data.archive_file.auth_func_src \
-  module.gcloud.data.google_firebase_android_app.android_sha \
   module.gcloud.data.google_firebase_android_app_config.android_config \
+  module.gcloud.data.google_firebase_android_app.android_sha \
   module.gcloud.data.google_firebase_apple_app_config.ios_config \
   module.gcloud.data.google_firebase_web_app_config.web_app_config \
   module.gcloud.data.google_project.project \
   module.gcloud.google_apikeys_key.google_maps_android_api_key \
   module.gcloud.google_apikeys_key.google_maps_ios_api_key \
   module.gcloud.google_apikeys_key.web_google_maps_api_key \
-  module.gcloud.google_artifact_registry_repository.private_docker_repository \
-  module.gcloud.google_artifact_registry_repository.public_docker_repository \
   module.gcloud.google_artifact_registry_repository_iam_member.application_backend_repo_read_member \
   module.gcloud.google_artifact_registry_repository_iam_member.github_actions_private_repo_read_member \
   module.gcloud.google_artifact_registry_repository_iam_member.github_actions_private_repo_write_member \
   module.gcloud.google_artifact_registry_repository_iam_member.github_actions_public_repo_write_member \
   module.gcloud.google_artifact_registry_repository_iam_member.public_read \
-  module.gcloud.google_cloud_run_service.application_backend \
+  module.gcloud.google_artifact_registry_repository.private_docker_repository \
+  module.gcloud.google_artifact_registry_repository.public_docker_repository \
   module.gcloud.google_cloud_run_service_iam_member.allow_unauthenticated \
-  module.gcloud.google_cloudfunctions_function.auth_before_create \
-  module.gcloud.google_cloudfunctions_function.auth_before_sign_in \
+  module.gcloud.google_cloud_run_service.application_backend \
   module.gcloud.google_cloudfunctions_function_iam_member.auth_before_create \
   module.gcloud.google_cloudfunctions_function_iam_member.auth_before_sign_in \
+  module.gcloud.google_cloudfunctions_function.auth_before_create \
+  module.gcloud.google_cloudfunctions_function.auth_before_sign_in \
   module.gcloud.google_firebase_android_app.android_app \
   module.gcloud.google_firebase_apple_app.ios_app \
   module.gcloud.google_firebase_database_instance.moov \
   module.gcloud.google_firebase_project.moov \
   module.gcloud.google_firebase_web_app.web_app \
-  module.gcloud.google_firestore_database.default \
+  module.gcloud.google_firestore_database.production \
   module.gcloud.google_identity_platform_config.moov \
   module.gcloud.google_project_iam_custom_role.limited_service_user \
   module.gcloud.google_project_iam_member.firebase_admin_service_agent \
@@ -71,9 +84,6 @@ TF_STATE_ITEMS = \
   module.gcloud.google_project_service.google_services_maps["geocoding-backend.googleapis.com"] \
   module.gcloud.google_project_service.google_services_maps["maps-backend.googleapis.com"] \
   module.gcloud.google_project_service.google_services_maps["maps-ios-backend.googleapis.com"] \
-  module.gcloud.google_secret_manager_secret.firebase_admin_service_account_secret \
-  module.gcloud.google_secret_manager_secret.firebase_config_web \
-  module.gcloud.google_secret_manager_secret.web_google_maps_api_key \
   module.gcloud.google_secret_manager_secret_iam_member.application_backend_firebase_adminsdk_secret_read \
   module.gcloud.google_secret_manager_secret_iam_member.application_backend_firebase_web_uiconfig_secret_read \
   module.gcloud.google_secret_manager_secret_iam_member.application_backend_wep_googlemaps_api_key_secret_read \
@@ -81,25 +91,19 @@ TF_STATE_ITEMS = \
   module.gcloud.google_secret_manager_secret_version.firebase_adminsdk_secret_version \
   module.gcloud.google_secret_manager_secret_version.firebase_config_web_version \
   module.gcloud.google_secret_manager_secret_version.web_google_maps_api_key_version \
+  module.gcloud.google_secret_manager_secret.firebase_admin_service_account_secret \
+  module.gcloud.google_secret_manager_secret.firebase_config_web \
+  module.gcloud.google_secret_manager_secret.web_google_maps_api_key \
+  module.gcloud.google_service_account_key.firebase_admin_service_account_key \
+  module.gcloud.google_service_account_key.github_actions_key \
   module.gcloud.google_service_account.application_backend_cloud_run_sa \
   module.gcloud.google_service_account.firebase_admin_service_account \
   module.gcloud.google_service_account.github_actions \
-  module.gcloud.google_service_account_key.firebase_admin_service_account_key \
-  module.gcloud.google_service_account_key.github_actions_key \
-  module.gcloud.google_storage_bucket.bucket_gcf_source \
-  module.gcloud.google_storage_bucket.bucket_tf_state_eu_global \
   module.gcloud.google_storage_bucket_object.auth \
+  module.gcloud.google_storage_bucket.bucket_gcf_source \
   module.gcloud.null_resource.docker_auth \
-  module.gcloud.null_resource.docker_auth_public \
-  module.gcloud.module.cdn.google_compute_backend_bucket.public_cdn \
-  module.gcloud.module.cdn.google_compute_global_address.public_cdn \
-  module.gcloud.module.cdn.google_compute_global_forwarding_rule.public_cdn \
-  module.gcloud.module.cdn.google_compute_target_http_proxy.public_cdn \
-  module.gcloud.module.cdn.google_compute_url_map.url_map \
-  module.gcloud.module.cdn.google_storage_bucket.public_images \
-  module.gcloud.module.cdn.google_storage_bucket_iam_binding.web_public_image \
-  module.gcloud.module.cdn.google_storage_bucket_object.public_web_background_image
-  
+  module.gcloud.null_resource.docker_auth_public
+
 $(TF_STATE_ITEMS:%=%-show): $(GOOGLE_APPLICATION_CREDENTIALS)
 	@$(TERRAFORM) state show $(@:%-show=%)
 .PHONY: $(TF_STATE_ITEMS:%=%-show)
