3f9f208e76a6bdf68c5311bcfac0c1756d883af6
David Berichon <davidberich@gmail.com>
2025-05-30 07:58:55 +0000

    feat(dev-environment): enhance DevContainer and local development setup
    Short description: The commit introduces significant modifications to the .devcontainer and local development setup. Notably, it updates the devcontainer.json file to include additional ports and extensions necessary for application tests. The initialize-cmd.sh script is extensively updated to improve developer experience, enhance configurations options, and ensure smoother operation of different components. The Makefile and associated build files have been refined for more efficient handling of docker images, and the docker-compose.yml has been adjusted to better manage service dependencies.
    
    Body:
    The changes are aimed at improving the local development environment to support a broader range of tasks and provide a more streamlined experience for developers. They bring in new features like application test report generation, Flutter support in the devcontainer as well as remodeling of the initialization command script. These modifications ensure convenience, customization capabilities, and better resource management for the development process.
    
    Moreover, these alterations will allow for quicker bootstrapping of the project, easier debugging, and testing, as well as increase overall productivity. Developers should note the newly added configuration options in the initialize-cmd.sh and adjustments made to the Docker build processes using Makefile and dedicated build scripts.

--- local.mk
+++ local.mk
@@ -17,21 +17,6 @@ BUILDER_IMAGE_DOCKER_BUILDX_LOCAL_CACHE_READ = type=local,src=$(BUILDER_IMAGE_DO
 endif
 BUILDER_IMAGE_DOCKER_BUILDX_LOCAL_CACHE_WRITE= type=local,dest=$(BUILDER_IMAGE_DOCKER_BUILDX_LOCAL_CACHE)
 
-local-builder-image: $(BUILDER_IMAGE_DOCKER_BUILDX_LOCAL_CACHE) docker-buildx-setup
-	@$(DOCKER_BUILDX_BAKE) --print builder
-	@$(DOCKER_BUILDX_BAKE) --load builder
-.PHONY: local-builder-image
-
-local-builder-image-push: docker-buildx-setup
-	@$(DOCKER_BUILDX_BAKE) --print builder
-	@$(DOCKER_BUILDX_BAKE) --push builder
-.PHONY: local-builder-image-push
-
-local-builder-image-ci: docker-buildx-setup
-	@$(DOCKER_BUILDX_BAKE) --print builder-ci
-	@$(DOCKER_BUILDX_BAKE) --push builder-ci
-.PHONY: local-builder-image-ci
-
 local-docker-compose-dev-image-pull:
 	@$(LOCAL_DOCKER_COMPOSE) pull dev
 .PHONY: local-docker-compose-dev-image-pull
@@ -52,36 +37,15 @@ LOCAL_DOCKER_COMPOSE_SERVICES = \
   application-backend \
   application-tests
 
-local-docker-images-pull: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=docker-%-image-pull) local-docker-compose-dev-image-pull
-.PHONY: local-docker-images-pull
-
-local-docker-images-push: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=docker-%-image-push) local-builder-image-push
-.PHONY: local-docker-images-push
-
-$(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image): docker-buildx-setup
-	@$(DOCKER_BUILDX_BAKE) --print $(@:local-%-image=%)
-	@$(DOCKER_BUILDX_BAKE) --load $(@:local-%-image=%)
-.PHONY: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image)
-
-$(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-pull):
-	@$(LOCAL_DOCKER_COMPOSE) pull $(@:local-%-image-pull=%)
-.PHONY: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-pull)
-
-$(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-push):
-	@$(DOCKER_BUILDX_BAKE) --print $(@:local-%-image-push=%)
-	@$(DOCKER_BUILDX_BAKE) --push $(@:local-%-image-push=%)
-.PHONY: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-push)
-
-$(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-ci): docker-buildx-setup
-	@$(DOCKER_BUILDX_BAKE) --print $(@:local-%-image-ci=%-ci)
-	@$(DOCKER_BUILDX_BAKE) --push $(@:local-%-image-ci=%-ci)
-.PHONY: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-ci)
-
 local-docker-compose-up: $(LOCAL_DOCKER_COMPOSE_SERVICES)
 .PHONY: local-docker-compose-up
 
 local-docker-compose-rm-all: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-docker-compose-rm)
 .PHONY: local-docker-compose-rm-all
+
+$(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-pull):
+	@$(LOCAL_DOCKER_COMPOSE) pull $(@:local-%-image-pull=%)
+.PHONY: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-image-pull)
 
 $(LOCAL_DOCKER_COMPOSE_SERVICES):
 	@$(MAKE) $(@:%=local-%-docker-compose-up)
@@ -113,7 +77,8 @@ $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-docker-compose-sh):
 .PHONY: $(LOCAL_DOCKER_COMPOSE_SERVICES:%=local-%-docker-compose-sh)
 
 -include local/android-studio/android-studio.mk
--include local/clarinet/clarinet.mk
+-include local/clarinet-devnet/clarinet-devnet.mk
 -include local/github/github.mk
 -include local/firebase-emulators/firebase-emulators.mk
--include local/vault/vault.mk
+-include local/vault-dev/vault-dev.mk
+-include local/application-tests/application-tests.mk
