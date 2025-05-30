668b2949d241be2b0cf24d50df8e3eecfeee457e
David Berichon <davidberich@gmail.com>
2025-05-30 07:27:50 +0000

    refactor(vault): rename 'vault' to 'vault-dev' and adjust related paths
    This commit renames all instances of 'vault' to 'vault-dev' in directory names, file names, and references within files. This includes adjustments to the docker-bake.hcl file where 'LATEST_BUILDER_IMAGE' is removed from the cache-from list, and changes to vault.mk where the path for 'docker-compose-up.sh' is updated to reflect the new 'vault-dev' naming. This refactoring ensures the code accurately reflects its development status and improves coherence across naming conventions in the project.

--- vault-dev/vault-dev.mk
+++ vault-dev/vault-dev.mk
@@ -6,7 +6,7 @@ endif
 VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE_WRITE= type=local,dest=$(VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE)
 
 local-vault-dev-docker-compose-up: local-vault-dev-docker-compose-rm
-	@VERSION=latest $(CURDIR)/local/vault/docker-compose-up.sh &
+	@VERSION=latest $(CURDIR)/local/vault-dev/docker-compose-up.sh &
 	@$(LOCAL_DOCKER_COMPOSE) logs vault-dev
 	@echo
 	@echo Started Androïd studio display: 
