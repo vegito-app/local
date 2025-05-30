93bcc0d2b6129840495499f025101e74757ab6b5
David Berichon <davidberich@gmail.com>
2025-05-30 07:55:36 +0000

    feat(firebase-emulators): update network settings and add storage service
    The network settings have been updated to redirect requests from ports 9150 and 9199 to 39150 and 39199 respectively. Check scripts including `docker-compose-up.sh` and `docker-emulators-start.sh` for details.
    
    Additionally, Firebase Emulator has been configured on 'localhost' to listen on ports 9150 and 9199 until the connections are available, ensuring a smoother connection establishment process. This change can be observed in the `docker-emulators-start.sh` script.
    
    A new service, 'storage', was added to the Firebase Emulators services list defined in `firebase-emulators.mk`. The configuration related to 'storage', such as host, port, rules, and storageBucket, has been added to `firebase.json`.
    
    This commit enhances our local development environment by adding more services and improving network configurations for Firebase emulators.

--- local/firebase-emulators/docker-emulators-start.sh
+++ local/firebase-emulators/docker-emulators-start.sh
@@ -23,19 +23,38 @@ bg_pids+=("$!")
 
 # Need localproxy to forward some required port that could not be 
 # configured to listen on 0.0.0.0 from firebase.json file.
+until nc -z localhost 4400; do
+    echo Waiting Firebase Emulator Reserved port at http://localhost:4400/ ;
+    sleep 1 ;
+done
 TARGET_PORT=4400 LISTEN_PORT=4401 localproxy &
 bg_pids+=("$!") 
+
 until nc -z localhost 4500; do
     echo Waiting Firebase Emulator Reserved port at http://localhost:4500/ ;
     sleep 1 ;
 done
 TARGET_PORT=4500 LISTEN_PORT=4501 localproxy &
 bg_pids+=("$!") 
+
+until nc -z localhost 9199; do
+    echo Waiting Firebase Emulator Reserved port 3 at http://localhost:9199/ ;
+    sleep 1 ;
+done
+TARGET_PORT=9199 LISTEN_PORT=39199 localproxy &
+
 until nc -z localhost 9299; do
     echo Waiting Firebase Emulator Reserved port 3 at http://localhost:9299/ ;
     sleep 1 ;
 done
 TARGET_PORT=9299 LISTEN_PORT=9399 localproxy &
+
+bg_pids+=("$!") 
+until nc -z localhost 9150; do
+    echo Waiting Firebase Emulator Reserved port 3 at http://localhost:9150/ ;
+    sleep 1 ;
+done
+TARGET_PORT=9150 LISTEN_PORT=39150 localproxy &
 bg_pids+=("$!") 
 
 sleep infinity
