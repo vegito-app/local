49817858f0a2f6d8ab3650e907d85aa1efedb2c0
David Berichon <davidberich@gmail.com>
2025-06-04 06:54:47 +0000

    feat(docker): update project environment variables and scripts for local Firebase emulators
    Description:
    This commit carries changes to the Docker compose file, updating project environment variables to accommodate new default IDs and files, and adds two new Pub/Sub topics related to image moderation. It also introduces a series of enhancements in the script files naming and handling, including renaming docker scripts to 'container', altering the Firebase emulators Dockerfile to use ENTRYPOINT over CMD, and changing the shell script to sleep indefinitely.
    
    The firebase-emulators `.mk` file sees additions for setting up and checking Pub/Sub emulator topics and subscriptions, along with enabling npm installation for a specific directory. This facilitates testing interactions with Google Cloud Pub/Sub in the local development environment.
    
    Changes also extend to firebase's JSON configuration, specifying Pub/Sub host, expanding authentication providers, and adding an ignore rule for the Firebase function's source directory.
    
    Overall, these modifications improve the flexibility and robustness of the development environment setup, providing more accurate emulation of the production environment.

--- local/firebase-emulators/firebase-emulators.mk
+++ local/firebase-emulators/firebase-emulators.mk
@@ -15,7 +15,7 @@ local-firebase-emulators-prepare: local-firebase-emulators-install local-firebas
 .PHONY: local-firebase-emulators-prepare
 
 local-firebase-emulators-install: gcloud-infra-auth-npm-install
-	# @cd $(CURDIR)/application/firebase/functions && npm install
+	@cd $(CURDIR)/application/firebase/functions && npm install
 .PHONY: local-firebase-emulators-install
 
 gcloud-infra-auth-npm-install:
@@ -51,9 +51,53 @@ local-firebase-emulators-docker-compose-up: local-firebase-emulators-docker-comp
 	@until nc -z firebase-emulators 4000 ; do \
 		sleep 1 ; \
 	done
-	$(LOCAL_DOCKER_COMPOSE) logs firebase-emulators
+	@$(MAKE) local-firebase-emulators-pubsub-init
+	@$(LOCAL_DOCKER_COMPOSE) logs firebase-emulators
 	@echo
 	@echo Started Firebase Emulator: 
 	@echo View Emulator UI at http://127.0.0.1:4000/
 	@echo Run "'make $(@:%-up=%-logs)'" to retrieve more logs
 .PHONY: local-firebase-emulators-docker-compose-up
+
+local-firebase-emulators-pubsub-wait:
+	@echo "⏳ Waiting for Pub/Sub emulator..."
+	@until nc -z localhost 8085; do \
+		echo "🕒 Waiting for port 8085..."; \
+		sleep 1; \
+	done
+	@echo "✅ Pub/Sub emulator is up!"
+.PHONY: local-firebase-emulators-pubsub-wait
+
+local-firebase-emulators-pubsub-init: local-firebase-emulators-pubsub-wait local-firebase-emulators-pubsub-topics-create local-firebase-emulators-pubsub-subscriptions
+.PHONY: local-firebase-emulators-pubsub-init
+
+
+LOCAL_FIREBASE_EMULATORS_PUB_SUB_TOPICS = \
+  $(LOCAL_FIREBASE_EMULATORS_PUBSUB_CREATED_IMAGES_TOPIC)
+
+local-firebase-emulators-pubsub-topics-create: $(LOCAL_FIREBASE_EMULATORS_PUB_SUB_TOPICS:%=local-firebase-emulators-pubsub-topics-create-%)
+
+$(LOCAL_FIREBASE_EMULATORS_PUB_SUB_TOPICS:%=local-firebase-emulators-pubsub-topics-create-%):
+	@echo "📣 Creating local Pub/Sub topic: $@"
+	curl -vv -X PUT http://localhost:8085/v1/projects/$(DEV_GOOGLE_CLOUD_PROJECT_ID)/topics/$(@:local-firebase-emulators-pubsub-topics-create-%=%)|echo
+.PHONY: $(LOCAL_FIREBASE_EMULATORS_PUB_SUB_TOPICS:%=local-firebase-emulators-pubsub-topics-create-%)
+
+# Use same PubSub topic to bypass current validation in local setup
+LOCAL_FIREBASE_EMULATORS_PUBSUB_VALIDATED_IMAGES_TOPIC = $(LOCAL_FIREBASE_EMULATORS_PUBSUB_CREATED_IMAGES_TOPIC)
+
+local-firebase-emulators-pubsub-subscriptions: $(LOCAL_FIREBASE_EMULATORS_PUB_SUB_SUBSCRIPTIONS:%=local-firebase-emulators-pubsub-subscriptions-create-%)
+
+$(LOCAL_FIREBASE_EMULATORS_PUB_SUB_SUBSCRIPTIONS:%=local-firebase-emulators-pubsub-subscriptions-create-%):
+	@echo "📣 Creating local Pub/Sub subscription: $@"
+	curl -vv -X PUT http://localhost:8085/v1/projects/$(DEV_GOOGLE_CLOUD_PROJECT_ID)/subscriptions/$(@:local-firebase-emulators-pubsub-subscriptions-create-%=%) \
+	  -H "Content-Type: application/json" \
+	  -d '{ "topic": "projects/$(DEV_GOOGLE_CLOUD_PROJECT_ID)/topics/$(LOCAL_FIREBASE_EMULATORS_PUBSUB_VALIDATED_IMAGES_TOPIC)" }'|echo
+.PHONY: $(LOCAL_FIREBASE_EMULATORS_PUB_SUB_SUBSCRIPTIONS:%=local-firebase-emulators-pubsub-subscriptions-create-%)
+
+local-firebase-emulators-pubsub-check:
+	@echo "📋 Listing local Pub/Sub topics:"
+	@curl -s http://localhost:8085/v1/projects/$(DEV_GOOGLE_CLOUD_PROJECT_ID)/topics | jq .
+	@echo
+	@echo "📋 Listing local Pub/Sub subscriptions:"
+	@curl -s http://localhost:8085/v1/projects/$(DEV_GOOGLE_CLOUD_PROJECT_ID)/subscriptions | jq .
+.PHONY: local-firebase-emulators-pubsub-check
