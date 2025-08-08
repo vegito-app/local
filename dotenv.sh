126052c71a82b4e045f146319f23e28e0631b639
David Berichon <david.berichon@vegito.app>
2025-08-08 07:06:44 +0000

    type: refactor(devcontainer): update Docker Compose configs and initialization
    1. Commented out the 'LOCAL_ANDROID_GPU_MODE' environment variables and related NVIDIA GPU settings in multiple locations, indicating a shift away from the usage of host GPU.
    2. Reordered some environment variables and removed redundant variable assignments for cleaner code.
    3. Updated service labels and port forwarding rules, including adding Android Debug Bridge (ADB) and modifying several VNC and X server settings.
    4. Substantially slimmed down the 'devcontainer.json' file in 'application/.devcontainer', removing large swaths of configuration code.
    5. Updated 'vault-dev.mk' to change how the VAULT_DEV_DIR is referred to during vault dev container setup.
    
    This refactor is likely part of an effort to streamline devcontainer settings and adapt the development environment to new requirements or constraints. Because some GPU-related settings were disabled, the use of graphics-intensive processes may be affected.

--- dotenv.sh
+++ dotenv.sh
@@ -115,7 +115,6 @@ services:
       LOCAL_ANDROID_STUDIO_ENV_SETUP=true
       LOCAL_ANDROID_STUDIO_APPIUM_EMULATOR_AVD_ON_START=true
       LOCAL_ANDROID_STUDIO_APK_PATH=mobile/build/app/outputs/flutter-apk/app-release.apk
-      LOCAL_ANDROID_STUDIO_ANDROID_GPU_MODE=host
       LOCAL_ANDROID_STUDIO_ANDROID_AVD_NAME=Pixel_8_Intel
 
     working_dir: ${PWD}/mobile
@@ -227,14 +226,14 @@ dockerComposeGpuOverride=${WORKING_DIR:-${PWD}}/.docker-compose-gpu-override.yml
 [ -f $dockerComposeGpuOverride ] || cat <<'EOF' > $dockerComposeGpuOverride
 services:
   android-studio:
-    environment:
-      LOCAL_ANDROID_GPU_MODE: ${LOCAL_ANDROID_STUDIO_ANDROID_GPU_MODE:-host}
-    devices:
-      - /dev/nvidia0
-    # runtime: nvidia # Uncomment this line if you are using the nvidia runtime.
-    runtime: runc
-    shm_size: "8gb"
-    group_add:
-      - sgx
+    # environment:
+    #   LOCAL_ANDROID_ANDROID_GPU_MODE=host
+    # devices:
+    #   - /dev/nvidia0
+    # # runtime: nvidia # Uncomment this line if you are using the nvidia runtime.
+    # runtime: runc
+    # shm_size: "8gb"
+    # group_add:
+    #   - sgx
 EOF
 
