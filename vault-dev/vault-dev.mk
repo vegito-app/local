0c1278604f5c6aea4673350134804e3a7bfa6552
David Berichon <david.berichon@vegito.app>
2025-08-08 07:06:44 +0000

    type: refactor(devcontainer): update Docker Compose configs and initialization
    1. Commented out the 'LOCAL_ANDROID_GPU_MODE' environment variables and related NVIDIA GPU settings in multiple locations, indicating a shift away from the usage of host GPU.
    2. Reordered some environment variables and removed redundant variable assignments for cleaner code.
    3. Updated service labels and port forwarding rules, including adding Android Debug Bridge (ADB) and modifying several VNC and X server settings.
    4. Substantially slimmed down the 'devcontainer.json' file in 'application/.devcontainer', removing large swaths of configuration code.
    5. Updated 'vault-dev.mk' to change how the VAULT_DEV_DIR is referred to during vault dev container setup.
    
    This refactor is likely part of an effort to streamline devcontainer settings and adapt the development environment to new requirements or constraints. Because some GPU-related settings were disabled, the use of graphics-intensive processes may be affected.

--- vault-dev/vault-dev.mk
+++ vault-dev/vault-dev.mk
@@ -1,5 +1,5 @@
-VAULT_DEV_DIR ?= $(LOCAL_DIR)/vault-dev
-LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE ?= $(VAULT_DEV_DIR)/.containers/docker-buildx-cache/vault-dev
+LOCAL_VAULT_DEV_DIR ?= $(LOCAL_DIR)/vault-dev
+LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE ?= $(LOCAL_VAULT_DEV_DIR)/.containers/docker-buildx-cache/vault-dev
 $(LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE):;	@mkdir -p "$@"
 ifneq ($(wildcard $(LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE)/index.json),)
 LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE_READ = type=local,src=$(LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE)
@@ -7,7 +7,7 @@ endif
 LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE_WRITE= type=local,mode=max,dest=$(LOCAL_VAULT_DEV_IMAGE_DOCKER_BUILDX_LOCAL_CACHE)
 
 local-vault-dev-container-up: local-vault-dev-container-rm
-	@LOCAL_VERSION=latest $(VAULT_DEV_DIR)/docker-compose-up.sh &
+	@LOCAL_VERSION=latest $(LOCAL_VAULT_DEV_DIR)/docker-compose-up.sh &
 	@$(LOCAL_DOCKER_COMPOSE) logs vault-dev
 	@echo
 	@echo Started Androïd studio display: 
