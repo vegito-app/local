4736f99595c4639ed795791faa335576f6e4ef00
David Berichon <davidberich@gmail.com>
2025-09-07 09:03:29 +0000

    refactor(makefiles): enhance parallelism and scripts across modules
    This commit modifies various Makefiles and related components to enhance the code maintainability, readability, and efficiency. The key changes are:
    
    1. Improved the pulling/pushing commands of Docker images process by introducing parallel execution. Instead of pulling/pushing images one-by-one, all images are now handled concurrently.
    2. Enhanced log outputs when starting/stopping the development environment and other services.
    3. Standardized 'local-vegito-backend-container-up' and 'vegito-backend-gcloud-images-delete-all' into 'vegito-backend-container-up' and 'vegito-backend-gcloud-images-delete-all' respectively for better consistency.
    4. Added new targets in local/android module's Makefile (such as checks, start/stop, logs etc.).
    5. Removed commented docker builder image in git.mk and updated docker-bake.hcl files for Android studio.
    6. Updated Dockerfile for local android studio to include appium-emulator-avd.sh script.
    7. Removed redundant pull/push command definitions from local.mk
    8. In vegito.mk, enhanced the image pull/push process with parallel execution and made some cleanup.
    
    The changes should significantly improve the speed of Docker operations as well as make the scripts clearer and more consistent.

--- android/android.mk
+++ android/android.mk
@@ -37,7 +37,7 @@ LOCAL_ANDROID_DOCKER_BUILDX_BAKE_IMAGES ?= \
 
 $(LOCAL_ANDROID_DOCKER_BUILDX_BAKE_IMAGES:%=local-android-%-image):
 	@echo Build configuration for $(@:%-image=%)
-	@$(LOCAL_DOCKER_BUILDX_BAKE) --print $(@:%-image=%)
+	$(LOCAL_DOCKER_BUILDX_BAKE) --print $(@:%-image=%)
 	@echo Building and loading the image for $(@:%-image=%)
 	@$(LOCAL_DOCKER_BUILDX_BAKE) --load $(@:%-image=%)
 .PHONY: $(LOCAL_ANDROID_DOCKER_BUILDX_BAKE_IMAGES:%=local-android-%-image)
@@ -53,28 +53,56 @@ LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES ?= \
   studio \
   appium
 
+$(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-rm): 
+	@$(MAKE) $(@:%-rm=%-stop)
+	@$(LOCAL_DOCKER_COMPOSE) rm -f $(@:local-%-container-rm=%)
+.PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-rm)
+
+$(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-start):
+	@-$(LOCAL_DOCKER_COMPOSE) start $(@:local-%-container-start=%) 2>/dev/null
+.PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-start)
+
+$(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-stop):
+	@-$(LOCAL_DOCKER_COMPOSE) stop $(@:local-%-container-stop=%) 2>/dev/null
+.PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-stop)
+
+$(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-logs):
+	@$(LOCAL_DOCKER_COMPOSE) logs $(@:local-%-container-logs=%)
+.PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-logs)
+
+$(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-logs-f):
+	@$(LOCAL_DOCKER_COMPOSE) logs --follow $(@:local-%-container-logs-f=%)
+.PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-logs-f)
+
+$(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-sh):
+	@$(LOCAL_DOCKER_COMPOSE) exec -it $(@:local-%-container-sh=%) bash
+.PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-container-sh)
+
 $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-pull):
 	@echo Pulling the image for $(@:local-%-image-pull=%)
 	@$(LOCAL_DOCKER_COMPOSE) pull $(@:local-%-image-pull=%)
 .PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-pull)
 
-local-android-dockercompose-images-pull: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-pull)
-.PHONY: local-android-dockercompose-images-pull
-
-local-android-docker-images-pull: 
-	@$(MAKE) -j local-android-dockercompose-images-pull
+local-android-docker-images-pull: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-pull)
 .PHONY: local-android-docker-images-pull
 
+local-android-docker-images-pull-parallel: 
+	@echo Pulling all android images in parallel...
+	@$(MAKE) -j local-android-docker-images-pull
+.PHONY: local-android-docker-images-pull-parallel
+
 $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-push):
+	@echo Pushing the image for $(@:local-%-image-push=%)
 	@$(LOCAL_DOCKER_COMPOSE) push $(@:local-%-image-push=%)
 .PHONY: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-push)
 
-local-android-dockercompose-images-push: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-push)
-.PHONY: local-android-dockercompose-images-push
-
-local-android-docker-images-push: 
-	@$(MAKE) -j local-android-dockercompose-images-push
+local-android-docker-images-push: $(LOCAL_ANDROID_DOCKER_COMPOSE_SERVICES:%=local-android-%-image-push)
 .PHONY: local-android-docker-images-push
+
+local-android-docker-images-push-parallel: 
+	@echo Pushing all android images in parallel...
+	@$(MAKE) -j local-android-docker-images-push
+.PHONY: local-android-docker-images-push-parallel
 
 local-android-appium-emulator-avd-wipe-data:
 	@$(LOCAL_ANDROID_STUDIO) bash -c ' \
