29be717a573a20b832fb1bc4e2b0b7ddb26d3aef
David Berichon <davidberich@gmail.com>
2025-09-28 08:25:34 +0000

    chore(build): streamline Makefile includes and update container operations
    Reorganized Makefile structure by including `nodejs.mk` and `go.mk`, harmonized container operation commands, and restructured image pull/push tasks. These changes improve maintainability and align naming conventions across build and deployment scripts. Removed redundant include statements and adjusted variable assignments for clarity in node module paths.

--- android/studio/studio.mk
+++ android/studio/studio.mk
@@ -1,5 +1,5 @@
 LOCAL_ANDROID_STUDIO_DIR ?= $(LOCAL_ANDROID_DIR)/studio
-
+LOCAL_ANDROID_STUDIO_IMAGE_LATEST ?= $(VEGITO_LOCAL_PUBLIC_IMAGES_BASE):android-studio-latest
 
 local-android-studio-container-up: local-android-studio-container-rm
 	VERSION=latest $(LOCAL_ANDROID_STUDIO_DIR)/docker-compose-up.sh &
