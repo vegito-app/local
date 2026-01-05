5a3857f7bcc28dec39dd0a5c56078eb3b684dbd8
Vegito Administrator <admin@vegito.app>
2026-01-05 02:40:56 +0000

    feat(ci): enhance GCS upload and metadata management in workflows
    Added new job "upload-release-to-gcs" to automate the uploading of release artifacts and metadata to Google Cloud Storage (GCS) for different environments (dev, staging, prod). This includes generating an index.json and uploading logs and test reports for the dev environment. Introduced new secrets and environment variables for GCS operations. Also updated the handling of release notes and removed an obsolete Makefile target to improve CI process efficiency. These changes aim to streamline artifact management and enhance traceability across environments.

--- robotframework/robotframework.mk
+++ robotframework/robotframework.mk
@@ -1,5 +1,5 @@
 LOCAL_ROBOTFRAMEWORK_TESTS_DIR ?= $(LOCAL_DIR)/example-application/tests
-
+LOCAL_ROBOTFRAMEWORK_TESTS_OUTPUT_DIR ?= $(LOCAL_ROBOTFRAMEWORK_TESTS_DIR)/output
 LOCAL_ROBOTFRAMEWORK_IMAGE_VERSION ?= $(VEGITO_LOCAL_PUBLIC_IMAGES_BASE):robotframework-$(VERSION)
 
 local-robotframework-container-up: local-robotframework-container-rm
@@ -13,8 +13,22 @@ local-robotframework-container-up: local-robotframework-container-rm
 
 ROBOT ?= $(LOCAL_DOCKER_COMPOSE) exec robotframework robot
 
+# local-robotframework-container-exec:
+# 	echo "📝 Running robotframework..."
+# 	mkdir -p $(LOCAL_ROBOTFRAMEWORK_TESTS_OUTPUT_DIR)
+# 	$(ROBOT) --outputdir $(LOCAL_ROBOTFRAMEWORK_TESTS_OUTPUT_DIR) robot
+# .PHONY: local-robotframework-container-exec
+
 local-robotframework-container-exec:
 	@echo "📝 Running robotframework..."
-	@$(ROBOT) --outputdir $(LOCAL_ROBOTFRAMEWORK_TESTS_DIR) robot
+	@$(LOCAL_DOCKER_COMPOSE) exec -e LOCAL_ROBOTFRAMEWORK_TESTS_DIR=$(LOCAL_ROBOTFRAMEWORK_TESTS_DIR) robotframework \
+	  robot --outputdir output robot
 .PHONY: local-robotframework-container-exec
 
+local-robotframework-container-exec-ci: local-dev-container-image-pull
+	@echo "Using dev container image: $(LOCAL_BUILDER_IMAGE)"
+	@$(LOCAL_DEV_CONTAINER_RUN) \
+	  make local-robotframework-container-exec \
+	    LOCAL_ROBOTFRAMEWORK_TESTS_DIR=$(LOCAL_ROBOTFRAMEWORK_TESTS_DIR)
+	@echo "End-to-end tests completed successfully in CI."
+.PHONY: local-robotframework-container-exec-ci
