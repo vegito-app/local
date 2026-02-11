cd302495c7fd03307f646b851009bc3d4d2604a9
David Berichon <davidberich@gmail.com>
2026-02-11 22:31:29 +0000

    refactor(build): remove hardcoded version in container startup scripts
    The commit removes the explicit 'VERSION=latest' assignment before executing container startup scripts across multiple makefiles. This change streamlines the process and ensures that the version management is handled dynamically or externally, thus improving flexibility and reducing the risk of inconsistencies due to hardcoding.

--- android/studio/studio.mk
+++ android/studio/studio.mk
@@ -1,7 +1,7 @@
 LOCAL_ANDROID_STUDIO_DIR ?= $(LOCAL_ANDROID_DIR)/studio
 
 local-android-studio-container-up: local-android-studio-container-rm
-	VERSION=latest $(LOCAL_ANDROID_STUDIO_DIR)/docker-compose-up.sh &
+	@$(LOCAL_ANDROID_STUDIO_DIR)/docker-compose-up.sh &
 	@$(LOCAL_DOCKER_COMPOSE) logs android-studio
 	@echo
 	@echo Started Androïd studio display: 
