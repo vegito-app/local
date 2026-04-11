0194e2c3c317ae4fb1a3c581fdd335dd91364f41
David Berichon <davidberich@gmail.com>
2026-04-11 13:43:00 +0000

    feat(firebase-emulators): enhance setup and initialization process
    The changes make the following updates to the Firebase emulators' setup and initialization:
    
    - A configuration JSON file is now generated during the Firebase emulators' installation.
    - Installation process also involves initializing and setting up data directories for specified Firebase services.
    - The initialization of Firebase emulators now includes creating designated data directories.
    - Improved logging and notifications during various parts of the emulator-setup process, providing more visibility into installation, initialization, and running steps such as starting Firebase Functions Emulator or Firebase Emulators container.

--- firebase-emulators/firebase-emulators.mk
+++ firebase-emulators/firebase-emulators.mk
@@ -8,33 +8,50 @@ LOCAL_FIREBASE_EMULATORS_AUTH_FUNCTIONS_DIR ?= $(LOCAL_DIR)/firebase-emulators/a
 LOCAL_FIREBASE_EMULATORS_DATA ?= $(LOCAL_DIR)/firebase-emulators/data
 LOCAL_FIREBASE_EMULATORS_CONFIG_JSON ?= $(LOCAL_DIR)/firebase-emulators/firebase.json
 
-local-firebase-emulators-install: local-firebase-emulators-auth-functions-npm-install
-	@cd $(LOCAL_FIREBASE_EMULATORS_AUTH_FUNCTIONS_DIR) && npm install
+local-firebase-emulators-config-json: $(LOCAL_FIREBASE_EMULATORS_CONFIG_JSON)
+.PHONY: local-firebase-emulators-config-json	
+
+$(LOCAL_FIREBASE_EMULATORS_CONFIG_JSON):
+	@echo "📝 Generating firebase.json configuration for Firebase emulators..."
+	@$(LOCAL_FIREBASE_EMULATORS_DIR)/firebase-emulators-config-create-json.sh
+
+local-firebase-emulators-install: \
+local-firebase-emulators-auth-functions-npm-install \
+local-firebase-emulators-config-json \
+local-firebase-emulators-data
 .PHONY: local-firebase-emulators-install
 
 local-firebase-emulators-auth-functions-npm-install:
-	cd $(LOCAL_FIREBASE_EMULATORS_AUTH_FUNCTIONS_DIR) && npm install
+	@echo "Installing dependencies in $(LOCAL_FIREBASE_EMULATORS_AUTH_FUNCTIONS_DIR)"
+	@cd $(LOCAL_FIREBASE_EMULATORS_AUTH_FUNCTIONS_DIR) && npm install
 .PHONY: local-firebase-emulators-auth-functions-npm-install
 
-local-firebase-emulators-init:
+local-firebase-emulators-init: local-firebase-emulators-data
+	@echo "Initializing Firebase emulators"
 	@$(FIREBASE_EMULATORS) init emulators 
 .PHONY: local-firebase-emulators-init
 
+FIREBASE_EMULATORS_SERVICES_LIST = $(shell echo $(FIREBASE_EMULATORS_SERVICES) | sed "s/,/ /g")
+
+LOCAL_FIREBASE_EMULATORS_DATA_DIRECTORIES = $(FIREBASE_EMULATORS_SERVICES_LIST:%=$(LOCAL_FIREBASE_EMULATORS_DATA)/%_export)
+
+local-firebase-emulators-data: $(LOCAL_FIREBASE_EMULATORS_DATA_DIRECTORIES)
+.PHONY: local-firebase-emulators-data
+
+$(LOCAL_FIREBASE_EMULATORS_DATA_DIRECTORIES):
+	@echo "🗂️ Creating Firebase emulators data directory at $@"
+	@mkdir -p $@
+
 local-firebase-emulators-functions-serve:
+	@echo "🚀 Starting Firebase functions emulator"
 	@cd $(LOCAL_FIREBASE_EMULATORS_DIR)/auth_functions && \
 	unset GOOGLE_APPLICATION_CREDENTIALS && \
 	npm run serve
 .PHONY: local-firebase-emulators-functions-serve
 
-local-firebase-emulators-config-json: $(LOCAL_FIREBASE_EMULATORS_CONFIG_JSON)
-.PHONY: local-firebase-emulators-config-json	
-
-$(LOCAL_FIREBASE_EMULATORS_CONFIG_JSON):
-	@echo "Creating Firebase emulators config JSON at $@"
-	@$(LOCAL_FIREBASE_EMULATORS_DIR)/firebase-emulators-config-create-json.sh
-
-local-firebase-emulators-start: local-firebase-emulators-install local-firebase-emulators-config-json
-	$(FIREBASE_EMULATORS) emulators:start \
+local-firebase-emulators-start: local-firebase-emulators-install
+	@echo "🚀 Starting Firebase emulators with services: $(FIREBASE_EMULATORS_SERVICES)"
+	@$(FIREBASE_EMULATORS) emulators:start \
 	  --project=$(GOOGLE_CLOUD_PROJECT_ID) \
 	  --import=$(LOCAL_FIREBASE_EMULATORS_DATA) \
 	  --export-on-exit $(LOCAL_FIREBASE_EMULATORS_DATA) \
@@ -46,7 +63,7 @@ local-firebase-emulators-docker-compose: local-firebase-emulators-prepare local-
 .PHONY: local-firebase-emulators-docker-compose
 
 local-firebase-emulators-container-up: local-firebase-emulators-container-rm
-	@echo "Starting mobile firebase-emulators container..."
+	@echo "🚀 Starting Firebase emulators container with image: $(LOCAL_FIREBASE_EMULATORS_IMAGE_VERSION)"
 	@$(LOCAL_FIREBASE_EMULATORS_DIR)/container-up.sh
 .PHONY: local-firebase-emulators-container-up
 
