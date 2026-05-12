7cf7481dfe244461099233c1e1b6f1c8d72a4edd
David Berichon <david.berichon@vegito.app>
2026-05-12 18:26:29 +0000

    prepare using vegito-app/docker as local subtree

--- docker/desktop-x/nvidia-gl-env.sh
+++
@@ -1,5 +0,0 @@
-
-# Force NVIDIA EGL vendor for XWayland GLX
-export GBM_BACKEND=nvidia-drm
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
