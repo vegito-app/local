2597a241539fb5311cb3e53ca994aeed617f7ef1
David Berichon <david.berichon@vegito.app>
2026-05-25 19:59:30 +0400

    feat(docker): ajouter un script de démarrage pour Clarinet Devnet et mettre à jour l'entrypoint

--- clarinet-devnet/entrypoint.sh
+++ clarinet-devnet/entrypoint.sh
@@ -2,24 +2,10 @@
 
 set -euo pipefail
 
-
-# 📌 List of PIDs of background processes
-bg_pids=()
-
-# 🧹 Function called at the end of the script to kill background processes
-kill_jobs() {
-    echo "🧼 Cleaning up background processes..."
-    for pid in "${bg_pids[@]}"; do
-        kill "$pid" || true
-        wait "$pid" 2>/dev/null || true
-    done
-}
-
-# 🚨 Register cleanup function to run on script exit
-trap kill_jobs EXIT
-
 if [ "${LOCAL_CLARINET_CONTAINER_INSTALL:-true}" = "true" ]; then
     clarinet-container-install.sh
 fi
 
-debian-dockerd-entrypoint.sh "$@"
+debian-docker-entrypoint.sh echo "🐳 Debian Rootless Docker - Setup done"
+
+exec "$@"
