712f947ef2fcb381e92a584eb6bb8b642bb2d933
David Berichon <david.berichon@vegito.app>
2026-05-29 15:51:44 +0400

    feat(devcontainer): refactor initialization and add local-container-start script
    This commit refactors the Dockerfile by adding a new container-install.sh which handles the local container cache setup and Emacs configuration persistence. This removes redundancy from entrypoint.sh as now it just calls devcontainer-install.sh for these tasks.
    
    Additionally, in docker-compose.yml, the 'sleep infinity' command has been replaced with 'local-container-start.sh'. It suggests the introduction of a new script for container startup operations.
    
    The changes are aimed to better separate concerns, streamline code execution, and possibly improve performance.

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -4,20 +4,9 @@ set -euo pipefail
 
 trap "echo Exited with code $?." EXIT
 
-# Local Container Cache
-local_container_cache=${local_container_cache:-${LOCAL_DIR:-${PWD}}/.containers/dev}
-mkdir -p $local_container_cache
+devcontainer-install.sh
 
-cat <<'EOF' >> ~/.bashrc
-alias e="emacs"
-EOF
+dev-entrypoint.sh echo "✅ Dev setup complete."
 
-# EMACS local configuration persistence
-# This allows you to persist your emacs configuration across container rebuilds.
-EMACS_DIR=${HOME}/.emacs.d
-[ -d $EMACS_DIR ] && mv $EMACS_DIR ${EMACS_DIR}_back
-mkdir -p ${local_container_cache}/emacs
-ln -sf ${local_container_cache}/emacs $EMACS_DIR
-
-
-dev-entrypoint.sh "$@"
+# Run the command
+exec "$@"
