7260c3da830c4a2f5087749a62b0682c75d058c7
David Berichon <david.berichon@vegito.app>
2026-06-04 18:05:42 +0200

    refactor(appium and clarinet-devnet): remove background process cleanup, update builder image name, remove healthcheck, enhance gcloud IAM docs
    - Removed the cleanup function for background processes in Appium's entrypoint.sh and adjusted the emulator setup execution flow.
    - Updated the builder image name in docker-bake.hcl for the target "local-clarinet-devnet-ci", "local-clarinet-devnet-latest-ci", and "local-clarinet-devnet".
    - Healthcheck from the docker-compose.yml file removed.
    - Added a detailed guide on managing `gcloud` identities and best practices to the gcloud/iam-model.md documentation. This includes information on service account key limits, solutions for common issues, and when to use specific commands for different scenarios.

--- android/appium/entrypoint.sh
+++ android/appium/entrypoint.sh
@@ -2,22 +2,6 @@
 
 set -euo pipefail
 
-# 📌 List of PIDs of background processes
-bg_pids=()
-
-# 🧹 Function called at the end of the script to kill background processes
-kill_jobs() {
-    echo "🧼 Cleaning up background processes..."
-    for pid in "${bg_pids[@]}"; do
-        kill "$pid" || true
-        wait "$pid" 2>/dev/null || true
-    done
-}
-
-# 🚨 Register cleanup function to run on script exit
-
-trap kill_jobs EXIT
-
 android_adb_key=${LOCAL_ANDROID_ADB_KEY_PATH:-~/.android/adbkey}
 android_adb_pubkey=${LOCAL_ANDROID_ADB_KEY_PUB_PATH:-~/.android/adbkey.pub}
 
@@ -29,5 +13,7 @@ else
     echo "[entrypoint] Existing ADB keypair detected, skipping generation."
 fi
 
-android-emulator-entrypoint.sh "$@"
+android-emulator-entrypoint.sh echo " [entrypoint] Android Emulator setup done"
+
+exec "$@"
 
