0c24ad5e5c9541597ace647830f0b205a548c291
David Berichon <david.berichon@vegito.app>
2026-08-31 12:30:56 +0000

    feat(pubsub): add vegetable images and order payment subscriptions to local emulator

--- firebase-emulators/pubsub.mk
+++ firebase-emulators/pubsub.mk
@@ -16,16 +16,30 @@ $(LOCAL_FIREBASE_EMULATORS_PUBSUB_TOPICS:%=local-firebase-emulators-pubsub-topic
 	@curl -X PUT http://$(LOCAL_FIREBASE_PUBSUB_EMULATOR_HOST)/v1/projects/$(GOOGLE_CLOUD_PROJECT_ID)/topics/$(@:local-firebase-emulators-pubsub-topics-create-%=%)|echo
 .PHONY: $(LOCAL_FIREBASE_EMULATORS_PUBSUB_TOPICS:%=local-firebase-emulators-pubsub-topics-create-%)
 
-local-firebase-emulators-pubsub-subscriptions: $(LOCAL_FIREBASE_EMULATORS_PUBSUB_SUBSCRIPTIONS:%=local-firebase-emulators-pubsub-subscriptions-create-%)
+local-firebase-emulators-pubsub-subscriptions: $(LOCAL_FIREBASE_EMULATORS_PUBSUB_TOPICS:%=local-firebase-emulators-%-pubsub-subscriptions)
 .PHONY: local-firebase-emulators-pubsub-subscriptions
 
-$(LOCAL_FIREBASE_EMULATORS_PUBSUB_SUBSCRIPTIONS:%=local-firebase-emulators-pubsub-subscriptions-create-%):
+local-firebase-emulators-vegetable-images-created-pubsub-subscriptions: $(VEGITO_APP_FIREBASE_PUBSUB_VEGETABLE_IMAGES_VALIDATED_SUBSCRIPTIONS:%=local-firebase-emulators-vegetable-images-created-pubsub-subscriptions-create-%)
+.PHONY: local-firebase-emulators-vegetable-images-created-pubsub-subscriptions
+
+$(VEGITO_APP_FIREBASE_PUBSUB_VEGETABLE_IMAGES_VALIDATED_SUBSCRIPTIONS:%=local-firebase-emulators-vegetable-images-created-pubsub-subscriptions-create-%):
 	@echo "📣 Creating local Pub/Sub subscription: $@"
-	@echo "📣 LOCAL_FIREBASE_EMULATORS_PUBSUB_SUBSCRIPTIONS: $(LOCAL_FIREBASE_EMULATORS_PUBSUB_SUBSCRIPTIONS)"
-	@curl -X PUT http://$(LOCAL_FIREBASE_PUBSUB_EMULATOR_HOST)/v1/projects/$(GOOGLE_CLOUD_PROJECT_ID)/subscriptions/$(@:local-firebase-emulators-pubsub-subscriptions-create-%=%) \
+	@echo "📣 VEGITO_APP_FIREBASE_PUBSUB_VEGETABLE_IMAGES_VALIDATED_SUBSCRIPTIONS: $(VEGITO_APP_FIREBASE_PUBSUB_VEGETABLE_IMAGES_VALIDATED_SUBSCRIPTIONS)"
+	curl -X PUT http://$(LOCAL_FIREBASE_PUBSUB_EMULATOR_HOST)/v1/projects/$(GOOGLE_CLOUD_PROJECT_ID)/subscriptions/$(@:local-firebase-emulators-vegetable-images-created-pubsub-subscriptions-create-%=%) \
 	  -H "Content-Type: application/json" \
-	  -d '{ "topic": "projects/$(GOOGLE_CLOUD_PROJECT_ID)/topics/$(LOCAL_FIREBASE_EMULATORS_PUBSUB_TOPICS)" }'|echo
-.PHONY: $(LOCAL_FIREBASE_EMULATORS_PUBSUB_SUBSCRIPTIONS:%=local-firebase-emulators-pubsub-subscriptions-create-%)
+	  -d '{ "topic": "projects/$(GOOGLE_CLOUD_PROJECT_ID)/topics/vegetable-images-created" }'|echo
+.PHONY: $(VEGITO_APP_FIREBASE_PUBSUB_VEGETABLE_IMAGES_VALIDATED_SUBSCRIPTIONS:%=local-firebase-emulators-vegetable-images-created-pubsub-subscriptions-create-%)
+
+local-firebase-emulators-order-payment-pubsub-subscriptions: $(VEGITO_APP_FIREBASE_PUBSUB_ORDER_PAYMENT_SUBSCRIPTIONS:%=local-firebase-emulators-order-payment-pubsub-subscriptions-create-%)
+.PHONY: local-firebase-emulators-order-payment-pubsub-subscriptions
+
+$(VEGITO_APP_FIREBASE_PUBSUB_ORDER_PAYMENT_SUBSCRIPTIONS:%=local-firebase-emulators-order-payment-pubsub-subscriptions-create-%):
+	@echo "📣 Creating local Pub/Sub subscription: $@"
+	@echo "📣 VEGITO_APP_FIREBASE_PUBSUB_ORDER_PAYMENT_SUBSCRIPTIONS: $(VEGITO_APP_FIREBASE_PUBSUB_ORDER_PAYMENT_SUBSCRIPTIONS)"
+	curl -X PUT http://$(LOCAL_FIREBASE_PUBSUB_EMULATOR_HOST)/v1/projects/$(GOOGLE_CLOUD_PROJECT_ID)/subscriptions/$(@:local-firebase-emulators-order-payment-pubsub-subscriptions-create-%=%) \
+	  -H "Content-Type: application/json" \
+	  -d '{ "topic": "projects/$(GOOGLE_CLOUD_PROJECT_ID)/topics/order-payment" }'|echo
+.PHONY: $(VEGITO_APP_FIREBASE_PUBSUB_ORDER_PAYMENT_SUBSCRIPTIONS:%=local-firebase-emulators-order-payment-pubsub-subscriptions-create-%)
 
 local-firebase-emulators-pubsub-wait:
 	@echo "⏳ Waiting for Pub/Sub emulator..."
